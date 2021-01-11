6a212d51099c5d2919c494019c98d15209dc81a5
saxenapeeyush <saxena.piyush.011@gmail.com>
2021-01-11 11:23:45 +0530

    added routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter , Route  } from 'react-router-dom';
+
+import Home from './views/Home';
+import States from './views/States';
+
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <BrowserRouter>
+
+        <Route path = "/" exact component = {Home}/>
+
+        <Route path = "/state/:stateName" component = {States}/>
+
+      </BrowserRouter>
+
     </div>
   );
 }
